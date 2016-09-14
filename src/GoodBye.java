7eeec1b7b72cf0b3f980cff9a4ac8b55501677b3
Karl Nicholas <knichola@mail.ccsf.edu>
2016-09-13 17:03:48 -0700

    Week3

--- src/GoodBye.java
+++ src/GoodBye.java
@@ -9,20 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class GoodBye extends HttpServlet {
-    String[] page = {
-            "<html>",
-            "<body>",
-            "<h1>Goodbye -- </h1>",    
-            "<a href='Hello' >Goto Hello</a>", 
-            "</body>",
-            "</html>",      
-          };
 
           protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-              PrintWriter writer = response.getWriter();
-              for ( String s: page) {
-                  writer.println(s);
-              }
-              
+      		request.getRequestDispatcher("WEB-INF/GoodBye.jsp").forward(request, response);
           }
 }
--- src/Hello.java
+++ src/Hello.java
@@ -8,21 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Hello extends HttpServlet {
-    String[] page = {
-      "<html>",
-      "<body>",
-      "<h1>Hello World -- </h1>",
-      "<a href='GoodBye' >Goto Goodbye</a>", 
-      "</body>",
-      "</html>",      
-    };
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        for ( String s: page) {
-            writer.println(s);
-        }
-        
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("name", "Java-EE-Struts2-MyAttr");
+		request.getRequestDispatcher("WEB-INF/Hello.jsp").forward(request, response);
     }
 
 }
