1d6235dfff0a6a3d5862590eacd3e0aa342e547d
Karl Nicholas <knichola@mail.ccsf.edu>
2016-09-21 06:10:38 -0700

    Remove compiler warnings.

--- src/GoodBye.java
+++ src/GoodBye.java
@@ -1,16 +1,16 @@
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class GoodBye extends HttpServlet {
 
-      protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   		request.getRequestDispatcher("WEB-INF/goodbye.jsp").forward(request, response);
       }
 
--- src/Index.java
+++ src/Index.java
@@ -1,12 +1,12 @@
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class Index extends HttpServlet {
 	
 	@Override
