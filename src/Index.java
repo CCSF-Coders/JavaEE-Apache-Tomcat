be6619142f90dd5ed8c42e2d749712bc645c8957
Karl Nicholas <knichola@mail.ccsf.edu>
2016-10-11 16:31:22 -0700

    remove excess.

--- src/Index.java
+++ src/Index.java
@@ -2,9 +2,7 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -22,19 +20,6 @@ public class Index extends HttpServlet {
 
 		try {
 			Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:mymemdb", "sa", "");
-
-		    Statement stmt = conn.createStatement();
-		    try {
-			    // Select first_name and last_name column from the customers table
-			    ResultSet rset = stmt.executeQuery ("select t.* from Registration t");
-			    rset.next();
-			    // Close the RseultSet
-			    rset.close();
-		    } finally {
-			    // Close the Statement
-			    stmt.close();
-		    }
-
 		    List<Registration> resistrations = new Registration().list(conn);
 			request.setAttribute("registrations", resistrations);
 			conn.close();
